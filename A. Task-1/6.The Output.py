204004ab9a6feb4eef30958098d4ac68aebd6253
Mohamed Ahmed Yousef <81932663+mohamedahmed-cloud@users.noreply.github.com>
2022-02-23 05:10:06 +0200

    comment add

--- A. Task-1/6.The Output.py
+++ A. Task-1/6.The Output.py
@@ -2,5 +2,5 @@ Number_of_questions=int(input())
 for i in range(Number_of_questions):
     input()
 
-for i in range(Number_of_questions):
+for i in range(Number_of_questions): #Same output for all questions
     print("gzuz")
