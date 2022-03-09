e489ba6ddb80ca7a2d54d172387a5651effd10c0
Mohamed Ahmed Yousef <81932663+mohamedahmed-cloud@users.noreply.github.com>
2022-03-09 18:57:57 +0200

    Finish Task -4

--- D.Task-4/4. Transmigration.py
+++ D.Task-4/4. Transmigration.py
@@ -1,38 +1,18 @@
-n, m, k = list(map(float, input().split()))
-n = int(n)
-m = int(m)
-k = list(k).split('.')[1]
-# print(k)
+#   input Operation
+first_line = input().split()
+n, m, k = int(first_line[0]), int(
+    first_line[1]), int(first_line[2].split('.')[1])
+#   OutPut Operation
 b = {}
-print(k.split(""))
-# for i in range(n):
-#     old_skills = input().split()
-#     c = int(old_skills[1])*k
-#     if c >= 100:
-#         b[old_skills[0]] = c
-# # print(b)
-# for _ in range(m):
-#     new_skills = input()
-#     if not (new_skills in b):
-#         b[new_skills] = 0
-
-# print(len(b))
-# # print(b)
-# for skills in sorted(b):
-#     print(skills, int(b[skills]))
-
-# ######################################################################
-a = input().split()
-n, m, k = int(a[0]), int(a[1]), int(a[2].split('.')[1])
-print(k)
-# b={}
-# for i in range(n):
-#     a=input().split()
-#     c=int(a[1])*k/100
-#     if c>=100: b[a[0]]=c
-# for i in range(m):
-#     d=input()
-#     if not (d in b): b[d]=0
-# print(len(b))
-# for i in sorted(b):
-#     print(i,int(b[i]))
+for i in range(n):
+    old_skill = input().split()
+    c = int(old_skill[1])*k/100
+    if c >= 100:
+        b[old_skill[0]] = c
+for i in range(m):
+    d = input()
+    if not (d in b):
+        b[d] = 0
+print(len(b))
+for i in sorted(b):
+    print(i, int(b[i]))
